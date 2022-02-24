93d932df0cf2175ef110c6ef15975962e47157a4
nemenezes <nemenezes@hotmail.com>
2022-02-24 22:08:22 +0000

    blacken

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/helloworld")
 async def hello_world():
     return "Hello World"
 
 
 @app.get("/add/")
-async def add_two_numbers(first: int=0, second: int=0):
+async def add_two_numbers(first: int = 0, second: int = 0):
     return first + second
 
 
 @app.get("/joinstrings/")
-async def join_strings(first_str: str="", second_str: str=""):
+async def join_strings(first_str: str = "", second_str: str = ""):
     return f"{first_str}-{second_str}"
