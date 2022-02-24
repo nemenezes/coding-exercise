622a121dc155ed565bfba9c0ef9d51ae75872c78
nemenezes <nemenezes@hotmail.com>
2022-02-24 22:57:32 +0000

    update api endpoints

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ app = FastAPI()
 
 @app.get("/helloworld")
 async def hello_world():
-    return "Hello World"
+    return {"msg": "Hello World"}
 
 
 @app.get("/add/")
 async def add_two_numbers(first: int = 0, second: int = 0):
-    return first + second
+    return {"sum": first + second}
 
 
 @app.get("/joinstrings/")
 async def join_strings(first_str: str = "", second_str: str = ""):
-    return f"{first_str}-{second_str}"
+    return {"output": f"{first_str}-{second_str}"}
---
+++ tests/test_main.py
@@ -0,0 +1,31 @@
+#  python -m pytest
+
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+
+def test_hello_world():
+    response = client.get("/helloworld")
+    assert response.status_code == 200
+    msg = response.json().get("msg", "")
+    assert msg == "Hello World"
+
+
+def test_add_two_numbers():
+    response = client.get("/add/?first=-4&second=67")
+    assert response.status_code == 200
+    result = response.json().get("sum", "")
+    assert result == 63
+
+
+def test_add_two_numbers_type_error():
+    response = client.get("/add/?first=4.5&second=9")
+    assert response.status_code == 422
+
+
+
+
+
