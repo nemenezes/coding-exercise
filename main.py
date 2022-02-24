019c4e65243028f6f37bb757db1cdf6227fa520c
nemenezes <nemenezes@hotmail.com>
2022-02-24 22:04:30 +0000

    add more endpoints to the api

--- main.py
+++ main.py
@@ -4,4 +4,14 @@ app = FastAPI()
 
 @app.get("/helloworld")
 async def hello_world():
-    return {"message": "Hello World"}
+    return "Hello World"
+
+
+@app.get("/add/")
+async def add_two_numbers(first: int=0, second: int=0):
+    return first + second
+
+
+@app.get("/joinstrings/")
+async def join_strings(first_str: str="", second_str: str=""):
+    return f"{first_str}-{second_str}"
